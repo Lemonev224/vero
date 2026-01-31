57e117a9ddb878cd00c75d31a41010be2efb6cbc
Arseniy Limonov <limonovarseniy11@gmail.com>
2026-01-31 08:51:21 +0000

    another fix attempt

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import ServicesSection from './components/Services';
 import Navbar from './components/navbar';
 
 export default function Home() {
